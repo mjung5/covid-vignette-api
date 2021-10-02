0c7e05654d8a67f06ae9270d0e008ffbdad94ff3
Min-Jung Jung <mjung5@ncsu.edu>
2021-10-01 23:20:22 -0400

    second commit

--- vignetteRender.R
+++ vignetteRender.R
@@ -5,7 +5,7 @@
 rmarkdown::render(
   input="covidVignette.Rmd",
   output_format="github_document",
-  output_file = "README.md"
+  output_file = "README.md",
   output_options=list(html_preview=FALSE, keep_html=FALSE)
 )
 
